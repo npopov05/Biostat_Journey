7427741c7cd0f07adb228025da642bbecea81629
Nik <npopov0507@gmail.com>
2025-07-13 11:36:06 +0400

    added random slope and compare two models

--- 02_scripts/day23_mixed_models.R
+++ 02_scripts/day23_mixed_models.R
@@ -22,3 +22,27 @@ model_intercept <- lmer(Reaction ~ Days + (1 | Subject), data = sleepstudy)
 
 # Get the summary
 summary(model_intercept)
+
+# Fit the random slope and intercept model
+model_slope <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy)
+
+# Get the summary
+summary(model_slope)
+
+# Get the AIC for both models
+AIC(model_intercept, model_slope)
+
+# Perform the likelihood ratio test
+anova(model_intercept, model_slope)
+
+# 1. The primary research question was to quantify the effect of sleep 
+# deprivation on reaction time.
+# 2. We fitted two mixed-effects models: one with a random intercept only,
+# and a more complex one with both a random intercept and a random slope 
+# for the effect of time. Both the AIC and the Likelihood Ratio Test (p < 0.001)
+# confirmed that the random slope model provided a significantly better fit to 
+# the data, indicating that individuals show different rates of change in their 
+#reaction time.
+# 3.  From the final model, the fixed effect for Days was 10.47 ms. 
+# This means that, on average, each additional day of sleep deprivation is 
+# associated with an increase of about 10.5 milliseconds in reaction time.
